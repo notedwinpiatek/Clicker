915fc64a072692c5af8999087a1b3d7da2172f95
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-03-31 14:40:00 +0200

    upgrade

---
+++ Clicker/Clicker/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.upgradeButton = new System.Windows.Forms.Button();
+            this.buttonLevelTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(326, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 143);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hit me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(326, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Money: $0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // upgradeButton
+            // 
+            this.upgradeButton.Location = new System.Drawing.Point(326, 402);
+            this.upgradeButton.Name = "upgradeButton";
+            this.upgradeButton.Size = new System.Drawing.Size(167, 36);
+            this.upgradeButton.TabIndex = 2;
+            this.upgradeButton.Text = "Upgrade ($10)";
+            this.upgradeButton.UseVisualStyleBackColor = true;
+            // 
+            // buttonLevelTextBox
+            // 
+            this.buttonLevelTextBox.Location = new System.Drawing.Point(326, 127);
+            this.buttonLevelTextBox.Name = "buttonLevelTextBox";
+            this.buttonLevelTextBox.ReadOnly = true;
+            this.buttonLevelTextBox.Size = new System.Drawing.Size(167, 23);
+            this.buttonLevelTextBox.TabIndex = 3;
+            this.buttonLevelTextBox.Text = "1";
+            this.buttonLevelTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonLevelTextBox);
+            this.Controls.Add(this.upgradeButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button upgradeButton;
+        private TextBox buttonLevelTextBox;
+    }
+}
---
+++ Clicker/Clicker/Form1.cs
@@ -0,0 +1,33 @@
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+        int cash;
+        int buttonLevel;
+        public Form1()
+        {
+            InitializeComponent();
+            cash = 0;
+            buttonLevel = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cash += (int)Math.Pow(10, buttonLevel-1);
+            label1.Text = "Money: $"+cash.ToString();
+        }
+
+        private void upgradeButton_Click(object sender, EventArgs e)
+        {
+            int upgradeCost = (int)Math.Pow(10, buttonLevel);
+            if (cash > upgradeCost)
+            {
+                buttonLevel++;
+                buttonLevelTextBox.Text = buttonLevel.ToString();
+                cash -= upgradeCost;
+                string nextUpgradeCost = "($" + Math.Pow(10, buttonLevel).ToString() + ")";
+                upgradeButton.Text = "Upgrade\n" + nextUpgradeCost;
+            }
+        }
+    }
+}
